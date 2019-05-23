532bf443a82980686b7adf64a7dad98db257d375
dianpw <dianpw@ymail.com>
2019-05-24 01:46:44 +0700

    koneksi database dan halaman login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,8 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
-		$this->load->view('auth/login');
+		$this->form_validation->set_rules('username', 'Username', 'required|trim');
+		$this->form_validation->set_rules('password', 'Password', 'required|trim');
+		
+		if ($this->form_validation->run() == false) {
+			$data['title'] = "SIAP Login";
+			$this->load->view('auth/login', $data);
+		}else{
+			echo "string";
+		}
 	}
 }

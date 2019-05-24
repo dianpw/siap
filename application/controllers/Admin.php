0e3ee540c11948c55fcbdea1e4f2432e2c24813e
dianpw <dianpw@ymail.com>
2019-05-24 22:54:21 +0700

    halaman admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,8 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin extends CI_Controller {
 
 	public function index(){
-		$data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
-		echo "selamat datang admin " . $data['user']['name'];
-	}
+		//$data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+		//echo "selamat datang admin " . $data['user']['name'];
+		$this->load->view('admin/index');
 
+
+	}
 }

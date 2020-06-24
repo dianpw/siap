1ce775134c8adae81ef33194108bf55c7e97f2aa
dianpw <dianpw@ymail.com>
2020-06-24 23:40:26 +0700

    recode controller login dan model user

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -26,49 +26,6 @@ class Auth extends CI_Controller {
     		$this->user->login();
 		}
 	}
-	private function _login(){
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$user = $this->db->get_where('user', ['username' => $username])->row_array();
-		if ($user) {
-			if ($user['is_active'] == 1) {
-					
-				if (md5($password) == $user['password']) {
-					$data = [
-						'username' => $user['username'],
-						'role_id' => $user['role_id']
-					];
-					$this->session->set_userdata($data);
-					if ($user['role_id'] == 1) {
-						# admin
-						redirect('admin');
-					}elseif ($user['role_id'] == 2) {
-						# akuntan
-						redirect('akuntan');
-					}elseif ($user['role_id'] == 3) {
-						# iklan
-						redirect('iklan');
-					}elseif ($user['role_id'] == 4) {
-						# koran
-						redirect('koran');
-					}else{
-						redirect('auth/logout');
-					}
-
-				}else{
-				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Password Salah</div>');
-				redirect('auth');
-
-				}
-			}else{
-				$this->session->set_flashdata('message', '<div class="alert alert-warning" role="alert"> User tidak aktif. silahkan hubungi Administrator</div>');
-				redirect('auth');
-			}
-		}else{
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Login Gagal, Silahkan Coba Lagi!</div>');
-			redirect('auth');
-		}
-	}
 
 	public function logout(){
 		$this->session->unset_userdata('username');
--- application/models/User.php
+++ application/models/User.php
@@ -5,14 +5,13 @@ class User extends CI_Model
 {
     public function login(){
         $post = $this->input->post();
-		//SELECT user.id_user, profile.username , role_account.id_role, role_account.role, user.email, user.password, membership.nama AS membership, status.valid_until, status.aktif
-		//FROM user
-		//INNER JOIN role_account ON role_account.id_role=user.status
-		//INNER JOIN status ON status.id_user=user.id_user
-		//INNER JOIN profile ON profile.id_user=user.id_user
-		//INNER JOIN membership ON membership.id_membership=status.status
-		//WHERE user.email='dianpw@ymail.com' AND status.valid_until >= now()
-		$this->db->select('user.id_user, profile.username , role_account.id_role, role_account.role, user.email, user.password, membership.nama AS membership, status.valid_until, status.aktif');
+		//SELECT account.id_account, account.username, login.password, role_account.role, profil.nama, profil.foto, profil.nrg
+		//FROM `login`
+		//INNER JOIN account ON account.id_login=login.id_login
+		//INNER JOIN role_account ON role_account.id_role=login.id_role
+		//INNER JOIN profil ON profil.id_account=login.id_login
+		//WHERE account.username='' AND login.password=''
+		$this->db->select('account.id_account, account.username, login.password, role_account.role');
 		$this->db->join('role_account', 'role_account.id_role=user.status');
 		$this->db->join('status', 'status.id_user = user.id_user');
 		$this->db->join('profile', 'profile.id_user=user.id_user');
